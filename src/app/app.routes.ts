782947ce3f3f6c32c03c2a688c3bebb59b1564e9
RohanKumar101 <rohitgoraimp@gmail.com>
2025-02-23 17:15:50 +0530

    newtable

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Table1Component } from './table1/table1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AppComponent },
+    { path: 'table1', component: Table1Component }, // Add this
+  ];
